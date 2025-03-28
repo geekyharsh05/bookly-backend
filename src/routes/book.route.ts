df82278951122eabf082c7ff1640f96014a90f5f
Harsh <hp424805@gmail.com>
2025-03-29 05:25:19 +0530

    refactor: update book routes to remove authentication middleware and enhance deleteBook route with ID parameter

--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { isAuthenticated } from "../middlewares/auth.middleware";
 import { BookController } from "../controllers/book.controller";
 
 const router = Router();
 const bookController = new BookController();
 
-router.post("/create-book", isAuthenticated, bookController.createBook);
-router.get("/get-books", isAuthenticated, bookController.getBooks)
-router.delete("/delete-book", isAuthenticated, bookController.deleteBook)
+router.post("/create-book", bookController.createBook);
+router.get("/get-books", bookController.getBooks)
+router.get("/get-books-recommendation", bookController.getBooksRecommendation)
+router.delete("/delete-book/:id", bookController.deleteBook)
 
 export default router;
