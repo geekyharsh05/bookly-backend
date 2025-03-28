af45b2fc2a1f5f3c676bc2e7b90b819ff190f9cc
Harsh <hp424805@gmail.com>
2025-03-29 05:11:15 +0530

    feat: add deleteBook method and update getBooks limit in BookController; implement image upload and deletion functions in Cloudinary module

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -11,6 +11,7 @@ export class BookController {
 
   public createBook = asyncHandler(async (req: Request) => {
     const result = await this.bookService.createBook(req.body);
+
     return {
       message: 'Book created successfully',
       ...result,
@@ -19,7 +20,7 @@ export class BookController {
 
   public getBooks = asyncHandler(async (req: Request) => {
     const page = parseInt(req.query.page as string) || 1;
-    const limit = parseInt(req.query.limit as string) || 10;
+    const limit = parseInt(req.query.limit as string) || 5;
 
     const result = await this.bookService.getBooks(page, limit);
 
@@ -28,4 +29,20 @@ export class BookController {
       ...result,
     };
   });
+
+  public deleteBook = asyncHandler(async (req: Request) => {
+    const userId = req.user?._id?.toString();
+
+    const result = await this.bookService.deleteBook({
+      id: req.params.id,
+      userId
+    });
+
+    return {
+      message: 'Book deleted successfully',
+      ...result,
+    };
+  });
 }
+
+
--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import "dotenv/config";
+import { ApiError } from '../utils/apierror.util';
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,4 +8,26 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
+export const uploadImageToCloudinary = async (image: string): Promise<string> => {
+    try {
+      const uploadResult = await cloudinary.uploader.upload(image);
+      return uploadResult.secure_url;
+    } catch (error) {
+      throw new ApiError(500, 'Image upload failed');
+    }
+};
+
+export const deleteImageFromCloudinary = async (imageUrl: string): Promise<void> => {
+    try {
+      const publicId = imageUrl.split('/').pop()?.split('.')[0];
+      if (!publicId) {
+        throw new ApiError(400, 'Invalid Cloudinary image URL');
+      }
+      
+      await cloudinary.uploader.destroy(publicId);
+    } catch (error) {
+      throw new ApiError(400, 'Image deletion failed');
+    }
+};
+  
 export default cloudinary;
