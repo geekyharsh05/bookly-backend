4db1e676159dbccbc80548752baf74e58bf4d915
Harsh <hp424805@gmail.com>
2025-03-29 05:25:27 +0530

    feat: add authentication middleware to book routes for enhanced security

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import AuthRoutes from "./auth.route"
 import BookRoutes from "./book.route"
+import { isAuthenticated } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
-router.use("/book", BookRoutes);
+router.use("/book", isAuthenticated, BookRoutes);
 
 export default router;
