46491381e2a0a12a0e6b80b7d358057dd41ed252
Harsh <hp424805@gmail.com>
2025-03-30 06:06:29 +0530

    feat: increase request body size limits for JSON and URL-encoded data in Express middleware

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,8 @@ const PORT = process.env.PORT ?? 3001;
 // cron job runs every 14 minutes.
 job.start();
 
-app.use(express.json());
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.use(morgan("dev"));
 app.use(helmet());
 
